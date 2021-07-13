7c360e8e1230a298dadf71dbc31def24a90c6f76
bstoverink <bstoverink@gmail.com>
2021-07-12 19:22:18 -0500

    part 2 completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 ## Part 1: Test it with SQL
-
+id	int
+employer	varchar(255)
+name	varchar(50)
+skill	varchar(255)
+skills	varchar(255)
 ## Part 2: Test it with SQL
-
+SELECT * FROM employer WHERE location = "St. Louis"
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
